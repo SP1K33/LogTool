fb69522b2924a289d6126978b497fd4349e718dd
Daniil Solyanoy <39529339+SP1K33@users.noreply.github.com>
2020-10-18 06:11:20 +0300

    Added logging

---
+++ LogTool/LogTool.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace SP1K3.LogTool
+{
+	public static class LogTool
+	{
+		private static LogToolConfig _currentConfig;
+
+		public static void Configure(LogToolConfig config)
+		{
+			_currentConfig = config;
+		}
+
+		private static bool TrySeparate(ref string source)
+		{
+			if (string.IsNullOrEmpty(source))
+			{
+				return false;
+			}
+
+			var result = source[source.Length - 1] != ' ';
+			if (result)
+			{
+				source += " ";
+			}
+			return result;
+		}
+
+		public static void Log(string log, LogToolConfig? overrideLog = null)
+		{
+			var config = overrideLog ?? _currentConfig;
+			string result = string.Empty;
+
+			if (FlagsHelper.IsSet(config.Rules, LogRules.UtcNow))
+			{
+				result += $"[{DateTime.UtcNow}]";
+			}
+
+			TrySeparate(ref result);
+
+			if (FlagsHelper.IsSet(config.Rules, LogRules.MethodInfo))
+			{
+				var method = new StackTrace().GetFrame(1).GetMethod();
+				var className = method.ReflectedType.Name;
+				var methodName = method.Name;
+				result += $"[{className}.{methodName}]";
+			}
+
+			TrySeparate(ref result);
+
+			result += log + '\n';
+
+			File.AppendAllText(config.FilePath, result);
+
+			if (FlagsHelper.IsSet(config.Rules, LogRules.LogConsole))
+			{
+				Console.Write(result);
+			}
+		}
+	}
+}
---
+++ LogTool/LogToolConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SP1K3.LogTool
+{
+	[Flags] public enum LogRules { UtcNow = 1, MethodInfo, LogConsole }
+
+	public readonly struct LogToolConfig
+	{
+		public LogToolConfig(LogRules logRules, string filePath)
+		{
+			Rules = logRules;
+			FilePath = filePath;
+		}
+		public LogRules Rules { get; }
+		public string FilePath { get; }
+	}
+}
